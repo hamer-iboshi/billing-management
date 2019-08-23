5a7462ecad730180c6aac93bc36935b277897d55
hi15 <hi15@inf.ufpr.br>
2019-08-22 21:27:29 -0300

    Tests with csv

--- backend/src/controllers/ContractController.js
+++ backend/src/controllers/ContractController.js
@@ -1,6 +1,8 @@
-const Contract = require('../models/Contracts');
+const path = require('path');
 const fs = require('fs');
 const csv = require('fast-csv');
+const csvjson = require('csvjson');
+const Contract = require('../models/Contracts');
 
 const contracts = [], installments = [];
 
@@ -10,23 +12,61 @@ module.exports = {
         if (!('contracts' in req.files) && !('delayed_installments' in req.files)) {
             return res.status(400).send('No files were uploaded.');
         }
-        // open uploaded file
-        csv.parseFile(req.files.contracts.name)
-        .on("data", function (data) {
-            contracts.push(data); // push each row
-        })
-        .on("end", function () {
-            console.log(contracts)
-            // fs.unlinkSync(req.file.path);   // remove temp file
-            //process "fileRows" and respond
-        });
+        if (path.extname(req.files.contracts.name) === '.csv') {
+            // open uploaded file
+            // csv.parseFile(req.files.contracts.name)
+            // .on("data", function (data) {
+            //     contracts.push(data); // push each row
+            // })
+            // .on("end", function () {
+            //     console.log(contracts)
+            //     // fs.unlinkSync(req.file.path);   // remove temp file
+            //     //process "fileRows" and respond
+            // });
+        } else {
+            return res.status(400).send('Error, the file is not csv.');
+        }
         
-        return res.send('File uploaded!');
+
+        if (path.extname(req.files.delayed_installments.name) === '.csv') {
+            // open uploaded file
+            // csv.parseFile(req.files.delayed_installments.name)
+            // .on("data", function (data) {
+            //     contracts.push(data); // push each row
+            // })
+            // .on("end", function () {
+            //     console.log(contracts)
+            //     // fs.unlinkSync(req.file.path);   // remove temp file
+            //     //process "fileRows" and respond
+            // });
+        } else {
+            return res.status(400).send('Error, the file is not csv.');
+        }
+         
+        // req.files.contracts.data.on('data', (chunk) => {
+        //     // chunks.push(chunk); // push data chunk to array
+        //     console.log(chunk);
+        //     // We can perform actions on the partial data we have so far!
+        // });
+        console.log(req.files.delayed_installments.data)
+        // csv.parseStream(req.files.delayed_installments.data).on('error', error => console.error(error)).on('data', row => console.log(row)).on('end', rowCount => console.log(`Parsed ${rowCount} rows`));
+        const jsonObj = csvjson.toObject(req.files.delayed_installments.data.toString());
+        console.log(jsonObj[0]['contract_id']);
+        return res.send('Files uploaded!');
     },
     
     async store_upload(req, res, next){
         console.log(req.files)
         console.log(req.body)
+        // csv.parseFile(req.files.contracts.name)
+        // .on("data", function (data) {
+        //     contracts.push(data); // push each row
+        // })
+        // .on("end", function () {
+        //     console.log(contracts)
+        //     // fs.unlinkSync(req.file.path);   // remove temp file
+        //     //process "fileRows" and respond
+        // });
         return res.send('File uploaded!');
     }
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,29 +5,15 @@ const DelayedInstallment = require('./controllers/DelayedInstallmentController')
 const multer = require('multer');
 const routes = express.Router();
 
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, path.resolve( __dirname, '..', 'tmp', 'uploads'))
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname)
-    }
-});
-var upload = multer({ 
-    dest: path.resolve( __dirname, '..', 'tmp', 'uploads'), 
-    storage: storage,
-    fileFilter:  (req, file, cb) => {
-        if (path.extension(file.originalname) !== '.csv') {
-            return cb(new Error('Only CSVs are allowed!'))
-        }
-        cb(null, true)
-    }
-});
-
+var storage = multer.memoryStorage();
+var upload = multer({ dest: 'uploads/', fileFilter:  (req, file, cb) => {
+            if ((file.mimetype) !== 'text/csv') {
+                return cb(new Error('Only CSVs are allowed!'))
+            }
+            cb(null, true)
+        }});
 routes.post('/',ContractController.store);
-routes.post('/upload',upload.fields([
-    { name: 'contracts', maxCount: 1 },
-    { name: 'delayed_installments', maxCount: 1 }
-]),ContractController.store_upload);
+// routes.post('/upload',upload.any(),ContractController.store_upload);
+routes.post('/upload',upload.single('contracts'),ContractController.store_upload);
 
 module.exports = routes;
