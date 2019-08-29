cedb1f4e5efecbdfe7355ef8b5061192a940f751
hi15 <hi15@inf.ufpr.br>
2019-08-29 15:45:11 -0300

    Added list contracts and upload page

---
+++ frontend/src/Pages/Home.js
@@ -0,0 +1,92 @@
+import React,  { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import { Card } from 'react-materialize';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
+import api from '../services/api'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 650,
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+}));
+
+export default function SimpleTable() {
+  const classes = useStyles();
+  const [contracts, setContracts] = useState([]);
+
+  useEffect(() => {
+    async function loadContracts() {
+      const response = await api.get('/');
+      setContracts(response.data);
+    }
+
+    loadContracts();
+  });
+
+  async function destroyContract(id){
+    const response = await api.post('/'+id+'/destroy');
+  }
+
+  return (
+    <Card
+      title="Contracts listing"
+    >
+      <Paper className={classes.root}>    
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>external_id</TableCell>
+              <TableCell align="right">custumer_name</TableCell>
+              <TableCell align="right">customer_email</TableCell>
+              <TableCell align="right">customer_cpf</TableCell>
+              <TableCell align="right">loan_value</TableCell>
+              <TableCell align="right">payment_term</TableCell>
+              <TableCell align="right">reality_address</TableCell>
+              <TableCell align="right">actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {contracts.map(contract => (
+              <TableRow key={contract._id}>
+                <TableCell component="th" scope="row">
+                  {contract._id}
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {contract.customer_name}
+                </TableCell>
+                <TableCell align="right">{contract.customer_email}</TableCell>
+                <TableCell align="right">{contract.customer_cpf}</TableCell>
+                <TableCell align="right">{contract.loan_value}</TableCell>
+                <TableCell align="right">{contract.payment_term}</TableCell>
+                <TableCell align="right">{contract.realty_address}</TableCell>
+                <TableCell align="right">
+                <Button variant="contained" color="secondary" className={classes.button} onClick={() => destroyContract(contract._id)}>
+                  Delete
+                </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
+    </Card>
+  );
+}
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
+import { Row, Col } from 'react-materialize';
 
 import Upload from './Pages/Upload';
 import Home from './Pages/Home';
@@ -7,10 +8,14 @@ import ContractDetail from './Pages/ContractDetail';
 
 export default function Routes(){
     return (
-        <BrowserRouter>
-            <Route path="/" exact component={Upload}/>
-            <Route path="/contract" component={Home}/>
-            <Route path="/contract/:id" component={ContractDetail}/>
-        </BrowserRouter>
+        <Row>
+            <Col s={12}>
+                    <BrowserRouter>
+                        <Route path="/" exact component={Upload}/>
+                        <Route path="/contract" component={Home}/>
+                        <Route path="/contract/:id" component={ContractDetail}/>
+                    </BrowserRouter>
+            </Col>
+        </Row>
     );
 }
