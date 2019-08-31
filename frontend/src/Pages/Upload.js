514d77b4beba1c9cd44b9583f22255276c5420fe
Hamer Iboshi <hamer.iboshi@gmail.com>
2019-08-31 00:40:35 -0300

    Fixed upload files and redirect to index page

--- frontend/src/Pages/Upload.js
+++ frontend/src/Pages/Upload.js
@@ -3,16 +3,16 @@ import { Row, Col, Card, Button, Icon } from 'react-materialize';
 import api from '../services/api';
 
 function Upload({ history }) {
-  const [contracts, setContracts] = useState('');
-  const [delayedInstallments, setDelayedInstallments] = useState('');
+  const [contracts, setContracts] = useState(React.useRef());
+  const [delayedInstallments, setDelayedInstallments] = useState(React.useRef());
 
   async function handleSubmit(e){
-    console.log(e.target);
     e.preventDefault();
-    const response = await api.post('/', {
-        contracts,
-        delayedInstallments
-    });
+    const data = new FormData();
+    data.append('contracts', (contracts ? contracts : ''));
+    data.append('delayed_installments', (delayedInstallments ? delayedInstallments : ''));
+    console.log(contracts,delayedInstallments);
+    const response = await api.post('/',data);
     history.push('/contract');
   }
 // action="http://localhost:3333/" method="post" encType="multipart/form-data" 
@@ -25,7 +25,7 @@ function Upload({ history }) {
                     <div className="file-field input-field">
                         <div className="btn">
                             <span>Contracts File</span>
-                            <input type="file" name="contracts" value={contracts} onChange={ e => setContracts(e.target.value)}/>
+                            <input type="file" name="contracts" onChange={ e => setContracts(e.target.files[0])}/>
                         </div>
                         <div className="file-path-wrapper">
                             <input className="file-path validate" type="text"/>
@@ -34,7 +34,7 @@ function Upload({ history }) {
                     <div className="file-field input-field">
                         <div className="btn">
                             <span>Delayed Installments File</span>
-                            <input type="file" name="delayed_installments" value={delayedInstallments} onChange={ e => setDelayedInstallments(e.target.value)}/>
+                            <input type="file" name="delayed_installments" onChange={ e => setDelayedInstallments(e.target.files[0])}/>
                         </div>
                         <div className="file-path-wrapper">
                             <input className="file-path validate" type="text"/>
