7425a0ffe758c0debb7aaf22a4f9135d4c37d78c
Hamer Iboshi <hamer.iboshi@gmail.com>
2019-09-04 21:04:01 -0300

    Update backend route

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const api = axios.create({
-    baseURL: process.env.BASE_URL
+    baseURL: process.env.REACT_APP_API_URL
 });
 
 export default api;
